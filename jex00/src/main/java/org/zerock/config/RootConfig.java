2460816c03500f18db433a1c2d5f58579b0719d3
kimue24 <54491294+kimue24@users.noreply.github.com>
2020-09-17 13:26:38 +0900

    1
    1

--- jex00/src/main/java/org/zerock/config/RootConfig.java
+++ jex00/src/main/java/org/zerock/config/RootConfig.java
@@ -1,5 +1,9 @@
 package org.zerock.config;
 
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
