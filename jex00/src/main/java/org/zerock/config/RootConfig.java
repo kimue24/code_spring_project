6d3dff98bdb56c585c8d5f04e2c06f8539047b54
kimue24 <54491294+kimue24@users.noreply.github.com>
2020-09-18 13:17:57 +0900

    20200918
    1

--- jex00/src/main/java/org/zerock/config/RootConfig.java
+++ jex00/src/main/java/org/zerock/config/RootConfig.java
@@ -6,10 +6,24 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
-public class RootConfig {
-	protected Class<?>[] getRootConfigClasses() {
-		return new Class[] {RootConfig.class};
-	}
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 
+
+@Configuration
+@ComponentScan(basePackages = { "org.zerock.sample"})
+public class RootConfig {
+	
+	@Bean
+	public DataSource dataSource() {
+		HikariConfig hikariConfig = new HikariConfig();
+		hikariConfig.setDriverClassName("oracle.jdbc.driver.OracleDriver");
+		hikariConfig.setJdbcUrl("jdbc:oracle:thin:@localhost:1521:orcl");
+		hikariConfig.setUsername("book_ex");
+		hikariConfig.setPassword("book_ex");
+		
+		HikariDataSource dataSource = new HikariDataSource(hikariConfig);
+		
+		return dataSource();
+	}
 }
